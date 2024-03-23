bd3b929f27e67c6ade91a717da899cf8e69b1cd9
ThejusGSajan <thejusgsajan@outlook.com>
2024-03-23 11:26:23 +0530

    refactored basic ui function

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import streamlit as st
 
-def main():
+def session_init():
     if 'history' not in st.session_state:
         st.session_state['history'] = []
     if 'generated' not in st.session_state:
         st.session_state['generated'] = ["Ask me about your pdfs!"]
     if 'past' not in st.session_state:
         st.session_state['past'] = ["Hi!"]
-    
+
+def main():
+    session_init()
     st.title("Real-Time Retrieval Augmented Generation on PDF Documents")
-    st.sidebar.title("Document processing")
+    st.sidebar.title("Document Processing")
 
 if __name__ == "__main__":
     main()
