006b3249433061e5295d3e487cbcc625b6140df5
ThejusGSajan <thejusgsajan@outlook.com>
2024-04-09 23:19:29 +0530

    integrated finetuned local llm

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ import tempfile
 from PyPDF2 import PdfReader
 from langchain.docstore.document import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import LlamaCpp
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from streamlit_chat import message
 
 def session_init():
     if 'history' not in st.session_state:
@@ -12,6 +18,11 @@ def session_init():
         st.session_state['generated'] = ["Ask me about your pdfs!"]
     if 'past' not in st.session_state:
         st.session_state['past'] = ["Hi!"]
+
+def chat(query,chain,history):
+    result = chain({"question": query, "chat_history": history})
+    history.append((query, result["answer"]))
+    return result["answer"]
 
 def main():
     session_init()
@@ -43,7 +54,45 @@ def main():
             chunk_size=10000,
             chunk_overlap=20)
         chunks = splitter.split_documents(documents)
-        print(chunks)
+        # print(chunks)
+
+        embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
+                                           model_kwargs={'device': 'cpu'})
+        vector_db = FAISS.from_documents(chunks, embedding=embeddings)
+
+        llm = LlamaCpp(streaming = True,
+                       model_path="mistral-7b-instruct-v0.2.Q4_K_M.gguf",
+                       temperature=0.75,
+                       top_p=1,
+                       verbose=True,
+                       n_ctx=4096)
+    
+        memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+        chain = ConversationalRetrievalChain.from_llm(llm=llm, chain_type='stuff',
+                                                 retriever=vector_db.as_retriever(search_kwargs={"k": 2}),
+                                                 memory=memory)
+
+        # display chat interface
+        reply = st.container()
+        container = st.container()
+
+        with container:
+            with st.form(key='my_form', clear_on_submit=True):
+                input = st.text_input("Question:", placeholder="Ask me something about your PDF!",
+                                      key='input')
+                submit_button = st.form_submit_button(label='Generate')
+
+            if submit_button and input:
+                with st.spinner('Finding answers...'):
+                    output = chat(input, chain, st.session_state['history'])
+                st.session_state['past'].append(input)
+                st.session_state['generated'].append(output)
+        if st.session_state['generated']:
+            with reply:
+                for i in range(len(st.session_state['generated'])):
+                    message(st.session_state['past'][i], is_user=True, key=str(i)+'_user',avatar_style='avataaars')
+                    message(st.session_state['generated'][i], key=str(i), avatar_style='bottts')
 
 if __name__ == "__main__":
     main()
