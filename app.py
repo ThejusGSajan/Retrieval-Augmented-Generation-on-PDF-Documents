2978049f75d1602b23cad31817c949d56761c3c6
ThejusGSajan <thejusgsajan@outlook.com>
2024-04-12 09:05:59 +0530

    removed deprecated packages

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import tempfile
 from PyPDF2 import PdfReader
 from langchain.docstore.document import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.llms import LlamaCpp
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.llms import LlamaCpp
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from streamlit_chat import message
